f54bc983189504dc256523aa180464665b65c468
Capybara16 <laurentinokris16@gmail.com>
2024-11-04 18:03:14 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,9 @@
+// Declare variables to store class information
+let subjectTitle = "OOP";           
+let classSchedule = "1:00 PM - 2:00 AM, MF";
+let classroom = "Room 122";                 
+let classInstructor = "Mr. Quevas";          
+let studentName = "Kristine Duga Laurentino";               
+
+// Log the information to the console in the specified format
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
